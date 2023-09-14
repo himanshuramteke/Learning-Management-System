0212baea10d4da66df96087ff525f8d271ea4930
Himanshu Ramteke <himanshuramteke007@gmail.com>
2023-09-14 22:27:12 +0530

    Added Contact us page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 import HomePage from './Pages/HomePage';
 import AboutUs from './Pages/AboutUs';
+import Contact from './Pages/Contact';
 import CourseList from './Pages/Course/CourseList';
 import NotFound from './Pages/NotFound';
 import Signup from './Pages/Signup';
@@ -18,6 +19,7 @@ function App() {
       <Routes>
          <Route path="/" element={<HomePage />} ></Route> 
          <Route path="/about" element={<AboutUs />} ></Route> 
+         <Route path="/contact" element={<Contact />}></Route>
          <Route path="/signup" element={<Signup />}></Route>
          <Route path="/login" element={<Login />}></Route>
          <Route path="/courses" element={<CourseList />}></Route>
