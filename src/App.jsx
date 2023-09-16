a92192949c952bdff7205d04492c93532f9f835d
Himanshu Ramteke <himanshuramteke007@gmail.com>
2023-09-16 21:54:42 +0530

    Added denied page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import HomePage from './Pages/HomePage';
 import AboutUs from './Pages/AboutUs';
 import Contact from './Pages/Contact';
 import CourseList from './Pages/Course/CourseList';
+import Denied from './Pages/Denied';
 import NotFound from './Pages/NotFound';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
@@ -23,7 +24,7 @@ function App() {
          <Route path="/signup" element={<Signup />}></Route>
          <Route path="/login" element={<Login />}></Route>
          <Route path="/courses" element={<CourseList />}></Route>
-
+         <Route path="/denied" element={<Denied />}></Route>
 
          <Route path="/*" element={<NotFound />}></Route>
       </Routes>
