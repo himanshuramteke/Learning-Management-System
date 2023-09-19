4c20e7432bcfb02b709bd8441e33ffa554a09dfd
Himanshu Ramteke <himanshuramteke007@gmail.com>
2023-09-19 17:28:59 +0530

    Added razorpay slice

--- src/Pages/Course/CourseDescription.jsx
+++ src/Pages/Course/CourseDescription.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
 
-import HomeLayout from "../Layouts/HomeLayout";
+import HomeLayout from "../../Layouts/HomeLayout";
 
 function CourseDescription() {
 
