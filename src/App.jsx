01502766f223590df19b511f451b5b1938c296e8
Himanshu Ramteke <himanshuramteke007@gmail.com>
2023-09-25 11:37:25 +0530

    Added admin dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import CheckoutSuccess from './Pages/Payment/CheckoutSuccess';
 import CheckoutFailure from './Pages/Payment/CheckoutFailure';
 import Displaylectures from './Pages/Dashboard/DisplayLectures';
 import AddLecture from './Pages/Dashboard/Addlecture';
+import AdminDashboard from './Pages/Dashboard/AdminDashboard';
 
 
 function App() {
@@ -42,6 +43,7 @@ function App() {
          <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
            <Route path="/course/create" element={<CreateCourse />} />
            <Route path="/course/addlecture" element={<AddLecture />} />
+           <Route path="/admin/dashboard" element={<AdminDashboard />} />
          </Route>
 
          <Route element={<RequireAuth allowedRoles={["ADMIN", "USER"]} />}>
